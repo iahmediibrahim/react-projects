d819e4e33ca39900bd936aabad95dc879dbafdff
swethusiast <aitc.dev@gmail.com>
2021-11-28 13:24:22 +0200

    nav toggle done!

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { Fragment } from 'react';
+import Navbar from './Navbar';
 function App() {
-  return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
-  )
+    return (
+        <Fragment>
+            <Navbar> </Navbar>
+        </Fragment>
+    );
 }
 
-export default App
+export default App;
