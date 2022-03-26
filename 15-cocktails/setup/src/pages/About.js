77e633185ce307337f1f571d404f934b0601b56d
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-03-26 16:12:10 +0200

    cocktails error about

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia illo similique quo dicta pariatur vel! Dolor quibusdam doloribus atque nesciunt aliquid ratione, quis quisquam illo voluptate ad accusantium impedit dignissimos vero eveniet autem, ullam, expedita ut modi cumque reprehenderit cupiditate?</p>
+    </section>
   )
 }
 
