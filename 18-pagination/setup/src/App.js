21ce941b9486ffeb700f3b1b04bc006e6601908b
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-03 23:46:27 +0200

    start pagination project

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
+import React from 'react'
 import Follower from './Follower'
+import { useFetch } from './useFetch'
+// import Loading from './../../../23-quiz/setup/src/Loading';
 function App() {
-  return <h2>pagination starter</h2>
+	const { loading, data } = useFetch()
+	console.log(data)
+	return (
+		<main>
+			<section className='section-title'>
+				<h1>{loading ? 'loading ... ' : 'pagination'}</h1>
+				<div className='underline'></div>
+				<section className='followers'>
+					<div className='container'>
+						{data.map((follower) => (
+							<Follower key={follower.id} {...follower} />
+						))}
+					</div>
+				</section>
+			</section>
+		</main>
+	)
 }
 
 export default App
