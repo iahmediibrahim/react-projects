d8442ab5c932252adc447a808d43e81d96587883
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-15 16:53:09 +0200

    fetching data and provide values in context

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,11 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-console.log(API_ENDPOINT)
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-	return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState({ show: false, message: '' })
+	const [movies, setMovies] = useState([])
+	const [query, setQuery] = useState('batman')
+	const fetchMovies = async (url) => {
+		setLoading(true)
+		try {
+			const resp = await fetch(url)
+			const data = await resp.json()
+			if (data.Response === 'True') {
+				setMovies(data.Search)
+				setError({ show: false, message: '' })
+			} else {
+				setError({ show: true, message: data.Error })
+			}
+			setLoading(false)
+		} catch (error) {
+			console.log(error.message)
+		}
+	}
+	useEffect(() => {
+		fetchMovies(`${API_ENDPOINT}&s=${query}`)
+	}, [query])
+
+	return (
+		<AppContext.Provider
+			value={{
+				loading,
+				error,
+				movies,
+				setQuery,
+			}}>
+			{children}
+		</AppContext.Provider>
+	)
 }
 // make sure use
 export const useGlobalContext = () => {
