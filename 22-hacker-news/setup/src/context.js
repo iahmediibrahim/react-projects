89bf85c55442107e81f87cb6056fe23cebfde702
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-16 03:51:57 +0200

    fetching hits

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -1,11 +1,16 @@
 import React, { useContext, useEffect, useReducer } from 'react'
-import { SET_LOADING } from './actions'
+import { SET_LOADING, SET_STORIES } from './actions'
+// import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } from './actions'
 import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
 const initialState = {
 	loading: true,
+	hits: [],
+	query: 'react',
+	page: 0,
+	nbPages: 0,
 }
 
 const AppContext = React.createContext()
@@ -14,9 +19,18 @@ const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState)
 	const fetchStories = async (url) => {
 		dispatch({ type: SET_LOADING })
+		try {
+			const resp = await fetch(url)
+			const data = await resp.json()
+			dispatch({ type: SET_STORIES, payload: data })
+
+			console.log(data)
+		} catch (error) {
+			console.log(error.response)
+		}
 	}
 	useEffect(() => {
-		fetchStories()
+		fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
 	}, [])
 
 	return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -1,8 +1,11 @@
-import { SET_LOADING } from './actions'
+import { SET_LOADING, SET_STORIES } from './actions'
 const reducer = (state, action) => {
 	switch (action.type) {
 		case SET_LOADING:
 			return { ...state, loading: true }
+		case SET_STORIES:
+			const { hits, page, nbPages } = action.payload
+			return { ...state, loading: false, hits, page, nbPages }
 		default:
 			throw new Error(`no matching ${action.type}`)
 	}
