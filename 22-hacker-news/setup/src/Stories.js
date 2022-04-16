136833290b90d284fe24623e9cc8da873e9355b6
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-16 03:12:22 +0200

    setup loading

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  return <h2>stories component</h2>
+	const { loading } = useGlobalContext()
+	if (loading) return <div className='loading'></div>
+	return <h2>stories component</h2>
 }
 
 export default Stories
--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -1,10 +1,10 @@
-import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
-
-const reducer = () => {}
+import { SET_LOADING } from './actions'
+const reducer = (state, action) => {
+	switch (action.type) {
+		case SET_LOADING:
+			return { ...state, loading: true }
+		default:
+			throw new Error(`no matching ${action.type}`)
+	}
+}
 export default reducer
