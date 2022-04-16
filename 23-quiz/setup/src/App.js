b8698d5f0d2fe80ae8a1127714aa870535f2e5c3
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-17 00:15:25 +0200

    setupt default state values

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useGlobalContext } from './context'
-
-import SetupForm from './SetupForm'
 import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm'
+
+// import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+	const { waiting, loading, questions, index, correct } = useGlobalContext()
+	if (waiting) return <SetupForm />
+	if (loading) return <Loading />
+	return <main>quiz sssss</main>
 }
 
 export default App
